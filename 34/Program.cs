1569060fa40d7511803c9150f1f70ca7e8c76c83
Иванов Иван <ivanov@example.com>
2022-11-30 14:45:47 +0300

    Less4

---
+++ 34/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] RandomIntArray(int size=10,int min=0,int max=100)
+{
+    int[] a=new int[size];
+    Random random=new Random();
+    for(int i=0;i<size;i++)
+       a[i]=random.Next(min,max);
+    return a;
+}
+
+int SumPositive(int[] a)
+{
+    int s=0;
+    foreach(int el in a)
+    return s;
+}
+int SumNegative(int[] a)
+{
+    int s=0;
+    foreach(int el in a)
+    if (el<0) s=s+el;
+    return s;
+}
+void Print(int[] a)
+{
+    for(int i=0;i<a.Length;i++)
+    System.Console.Write($"{a[i],5}");
+}
+
+int[] a=RandomIntArray(2,-9,9);
+Print(a);
+System.Console.WriteLine();
+System.Console.WriteLine($"Сумма положительных {SumPositive(a)}");
+System.Console.WriteLine($"Сумма отрицательных {SumNegative(a)}");
---
+++ 39/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] RandomIntArray(int size=10,int min=0,int max=100)
+{
+    int[] a=new int[size];
+    Random random=new Random();
+    for(int i=0;i<size;i++)
+       a[i]=random.Next(min,max);
+    return a;
+}
+
+int SumOddPositions(int[] a)
+{
+    int s=0;
+    for(int i=0;i<a.Length;i+=2)
+    return s;
+}
+
+void Print(int[] a)
+{
+    for(int i=0;i<a.Length;i++)
+    System.Console.Write($"{a[i],5}");
+}
+
+int[] a=RandomIntArray(3,0,10);
+Print(a);
+System.Console.WriteLine();
+System.Console.WriteLine(SumOddPositions());
