4a18d48b09452d050e978603e2fc351f0b75d12a
Иванов Иван <ivanov@example.com>
2022-12-28 20:33:27 +0300

    less8

---
+++ 81/Program.cs
@@ -0,0 +1,11 @@
+﻿// 81. Вывести на экран символы от A до Z как показано ниже: 
+//A 
+//BB 
+//CCC 
+//...
+for(Char c='A';c<='Z';c++)
+{
+    for(int i=1;i<=c-'A'+1;i++)
+    System.Console.WriteLine(c);
+    System.Console.WriteLine();
+}
---
+++ 83/Program.cs
@@ -0,0 +1,7 @@
+﻿// 83. Подсчитать количество цифр среди вводимых с клавиатуры символов
+
+string s=Console.ReadLine();
+int k=0;
+for(int i=0;i<s.Length;i++)
+    if (char.IsDigit(s[i])) k++;
+    System.Console.WriteLine(k);
